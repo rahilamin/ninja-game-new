b08eee3183596691d23ffae1bb7681568504ca06
rahilamin <RAHILAMIN@ROCKETMAIL.COM>
2022-07-31 21:50:50 +0100

    created animations states
    5 states

--- Assets/TextMesh Pro/myPlayer.cs
+++ Assets/TextMesh Pro/myPlayer.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class myPlayer : MonoBehaviour
 {
+
+    public Animator animator;
+    public float speed = 0;
     // Start is called before the first frame update
     void Start()
     {
