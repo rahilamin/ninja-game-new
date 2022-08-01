88a4773f3905a26175f11d5b75634388a8d92e53
rahilamin <RAHILAMIN@ROCKETMAIL.COM>
2022-08-01 23:31:01 +0100

    ninja walks, kinda
    added a few better backgrounds, also added some lines of code that enables the player to run, needed this to add the different states
    
    Goodnight, we are one step closer

--- Assets/TextMesh Pro/myPlayer.cs
+++ Assets/TextMesh Pro/myPlayer.cs
@@ -3,19 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class myPlayer : MonoBehaviour
-{
+{ public float sideMax = 5f; //Try screen.width
+    public float midlimit = 1f;
 
     public Animator animator;
     public float speed = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Touch touch;
+    public Rigidbody2D rb ;
+  
+  void start()
+  {
+      rb = GetComponent<Rigidbody2D>();
+  }
+  
+
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                transform.position = new Vector2 (touch.deltaPosition.y*speed/4,
+                touch.deltaPosition.y*speed
+                    );
+            }
+          // Debug.Log();
+
+          //  transform.position = Vector2(0,speed*Time.deltaTime);
+        }
     }
 }
