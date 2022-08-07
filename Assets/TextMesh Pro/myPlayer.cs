fb762cdf61e40d78779c6869d56fa61a34859db1
TheDuday27 <72023504+TheDuday27@users.noreply.github.com>
2022-08-07 13:35:41 +0100

    Health bar integrated

--- Assets/TextMesh Pro/myPlayer.cs
+++ Assets/TextMesh Pro/myPlayer.cs
@@ -3,30 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class myPlayer : MonoBehaviour
-{ public float sideMax = 5f; //Try screen.width
+{
+    public float sideMax = 5f; //Try screen.width
     public float midlimit = 1f;
 
     public Animator animator;
     public float speed = 0;
     private Touch touch;
-    public Rigidbody2D rb ;
+    public Rigidbody2D rb;
 
     public Vector3 jump;
-    public float x,y,z;
-    
+    public float x, y, z;
+
     public float jumpForce = 2f;
-  
-  void start()
-  {
-      rb = GetComponent<Rigidbody2D>();
+
+    void start()
+    {
+        rb = GetComponent<Rigidbody2D>();
 
 
-      jump = new Vector3(x,y,z);
-      
-  }
-  
+        jump = new Vector3(x, y, z);
 
-    
+    }
 
     // Update is called once per frame
     void Update()
@@ -37,17 +35,13 @@ public class myPlayer : MonoBehaviour
 
             if (touch.phase == TouchPhase.Stationary)
             {
-               // transform.position = new Vector2 (touch.deltaPosition.y*speed/4,
-               // touch.deltaPosition.y*speed
-                        
-                     //rb.AddForce(jump,ForceMode2D.Impulse);
-                     rb.AddRelativeForce(jump,ForceMode2D.Impulse);
+                // transform.position = new Vector2 (touch.deltaPosition.y*speed/4,
+                // touch.deltaPosition.y*speed
+
+                //rb.AddForce(jump,ForceMode2D.Impulse);
+                rb.AddRelativeForce(jump, ForceMode2D.Impulse);
 
             }
-            }
-          // Debug.Log();
-
-          //  transform.position = Vector2(0,speed*Time.deltaTime);
         }
     }
-
+}
